9edafd2fede75f592c832432d2fc2859326db9fc
erlangxk <erlang@viossolution.com>
2014-05-06 18:48:04 +0800

    add dealer connected/disconnected message handling

--- src/baccarat_game.erl
+++ src/baccarat_game.erl
@@ -29,6 +29,12 @@ stop_bet()->
 commit(Cards)->
 	gen_fsm:sync_send_event(?MODULE,{commit,Cards}).
 
+dealer_connected(Dealer)->
+	gen_fsm:sync_send_all_state_event(?MODULE,{dealer_connected,Dealer}).
+
+dealer_disconnected(Pid)->
+	gen_fsm:send_all_state_event(?MODULE,{dealer_disconnected,Pid}).
+
 stopped(start_bet,_From,State=#state{ticker=Ticker})->
 	%%start to count down
 	case Ticker of
@@ -73,15 +79,34 @@ handle_info(tick,betting,State=#state{ticker=Ticker})->
 			NewState=State#state{ticker={TRef,Value-1}},
 			{next_state,betting,NewState}
 	end;
-
 handle_info(Info,StateName,State)->
 	lager:error("unexpected handle_info, info ~p,stateName ~p,state ~p",[Info,StateName,State]),
 	{next_state,StateName,State}.
 
+handle_event(Event={disconnect,Pid},StateName,State=#state{dealer=DealerNow})->
+	lager:info("unexpected handle_event, event ~p,stateName ~p,state ~p",[Event,StateName,State]),
+	NewState=case DealerNow of
+		{Pdealer,_Dealer} when Pdealer==Pid->
+			State#state{dealer=undefined};
+		_ -> 
+			State
+	end,
+	{next_state,StateName,NewState};
 handle_event(Event,StateName,State)->
 	lager:error("unexpected handle_event, event ~p,stateName ~p,state ~p",[Event,StateName,State]),
 	{next_state,StateName,State}.
 
+handle_sync_event(Event={dealer_connected,Dealer},From={Pid,_},StateName,State=#state{dealer=DealerNow})->
+	lager:info("dealer_connected, event ~p,from ~p,info ~p,stateName ~p,state ~p",[Event,From,StateName,State]),
+	{Result,NewState}=case DealerNow of
+		undefined->
+		   	State2=State#state{dealer={Pid,Dealer}},
+		   	%% should monitor this Pid, when it dies, disconnect the dealer
+			{ok,State2};
+		_ ->
+			{{error,dealer_existed},State}
+	end,
+	{reply,Result,StateName,NewState};
 handle_sync_event(Event,From,StateName,State)->
 	lager:error("unexpected handle_sync_event, event ~p,from ~p,info ~p,stateName ~p,state ~p",[Event,From,StateName,State]),
 	{next_state,StateName,State}.
