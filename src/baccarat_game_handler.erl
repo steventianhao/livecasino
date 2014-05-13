22dbf61b9e1636a7050b050c7ee23d9687b73b80
erlangxk <simon@simonbmp.local>
2014-05-13 17:25:10 +0800

    add game handler

---
+++ src/baccarat_game_handler.erl
@@ -0,0 +1,39 @@
+-module(baccarat_game_handler).
+-behavior(gen_event).
+
+-record(state,{listener}).
+-include("dealer.hrl").
+
+-export([init/1,handle_event/2,handle_call/2,code_change/3,terminate/2,handle_info/2]).
+
+dealer_to_json(Dealer)->
+	#{dealer=>list_to_binary(Dealer#dealer.name)}.
+
+init(Pid)->
+	{ok,#state{listener=Pid}}.
+
+dealer_event(Kind,Dealer,Pid)->
+	M=maps:merge(#{kind=>Kind},dealer_to_json(Dealer)),
+	Pid ! {json,jsx:encode(M)}.
+	
+
+handle_event({dealer_connect,Dealer},State=#state{listener=Pid})->
+	dealer_event(dealer_disconnect,Dealer,Pid),
+	{ok,State};
+
+handle_event({dealer_disconnect,Dealer},State=#state{listener=Pid})->
+	dealer_event(dealer_disconnect,Dealer,Pid),
+	{ok,State}.
+
+code_change(_OldVsn,State,_Extra)->
+	{ok,State}.
+
+handle_call(_Request,State)->
+	{ok,ok,State}.
+
+handle_info(_Info,State)->
+	{ok,State}.
+
+terminate(_Arg,_State)->
+	ok.
+
