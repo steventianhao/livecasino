0e3f1d84032d642e503b6ded7446902f0edc9036
erlangxk <erlang@viossolution.com>
2014-05-20 18:45:50 +0800

    finish the round db function

--- src/mysql/mysql_db.erl
+++ src/mysql/mysql_db.erl
@@ -1,40 +1,39 @@
 -module(mysql_db).
 
--record(dbNewRound,{shoeIndex,roundIndex,dealerId,dealerTableId,createTime}).
--record(dbRound,{id,shoeIndex,roundIndex,dealerId,dealerTableId,createTime,status,finish_time,cards}).
+-record(dbNewRound,{shoe_index,round_index,dealer_id,dealer_table_id,create_time}).
+-record(dbRound,{id,shoe_index,round_index,dealer_id,dealer_table_id,create_time,status,finish_time,cards}).
 
 -export([add_pool/0]).
 -export([insert_round/2,update_round/2,update_round/4,load_last_round/2]).
-
+-define(CONN,casino_db_pool).
 
 add_pool()->
-	emysql:add_pool(casino_pool, [{size,1},
-				     {user,"ts1"},
-				     {host,"192.168.1.12"},
-				     {password,"111111"},
-				     {database,"casino"},
-				     {encoding,utf8}]),
-	casino_pool.
-
+	Params=[{size,1},{user,"ts1"},{host,"192.168.1.12"},{password,"111111"},{database,"casino"},{encoding,utf8}],
+	emysql:add_pool(?CONN,Params).
 
 insert_round(Conn,DbNewRound)->
-	Sql = <<"insert into rounds (shoe_index,round_index,dealer_id,dealer_table_id,create_time) values (?,?,?,?,?)">>,
+	Sql = <<"insert into rounds (shoe_index,round_index,dealer_id,dealer_table_id,create_time,status) values (?,?,?,?,?,1)">>,
 	emysql:prepare(stmt_insert_round,Sql),
-	#dbNewRound{shoeIndex=ShoeIndex,roundIndex=RoundIndex,dealerId=DealerId,dealerTableId=DealerTableId,createTime=CreateTime}=DbNewRound,
-	emysql:execute(Conn,stmt_insert_round,[ShoeIndex,RoundIndex,DealerId,DealerTableId,CreateTime]).
+	#dbNewRound{shoe_index=ShoeIndex,round_index=RoundIndex,dealer_id=DealerId,dealer_table_id=DealerTableId,create_time=CreateTime}=DbNewRound,
+	Result=emysql:execute(Conn,stmt_insert_round,[ShoeIndex,RoundIndex,DealerId,DealerTableId,CreateTime]),
+	emysql:insert_id(Result).
 
 update_round(Conn,Id)->
-	Sql = <<"update rounds set status=2 where id = ?">>,
+	Sql = <<"update rounds set status=2 where id = ? and status=1">>,
 	emysql:prepare(stmt_update_round1,Sql),
-	emysql:execute(Conn,stmt_update_round1,[Id]).
+	Result=emysql:execute(Conn,stmt_update_round1,[Id]),
+	emysql:affected_rows(Result).
+
 
 update_round(Conn,Id,Cards,FinishTime)->
-	Sql = <<"update rounds set status=3,cards=?,finish_time=? where id= ?">>,
+	Sql = <<"update rounds set status=3,cards=?,finish_time=? where id= ? and status =2">>,
 	emysql:prepare(stmt_update_round2,Sql),
-	emysql:execute(Conn,stmt_update_round2,[Cards,FinishTime,Id]).
+	Result=emysql:execute(Conn,stmt_update_round2,[Cards,FinishTime,Id]),
+	emysql:affected_rows(Result).
 
 load_last_round(Conn,DealerTableId)->
 	Sql = <<"select id,shoe_index,round_index,dealer_id,dealer_table_id,create_time,status,finish_time,cards from rounds where dealer_table_id =? order by id desc limit 1">>,
-	emysql:parepare(stmt_load_last_round,Sql),
-	emysql:execute(Conn,stmt_load_last_round,[DealerTableId]).
+	emysql:prepare(stmt_load_last_round,Sql),
+	Result=emysql:execute(Conn,stmt_load_last_round,[DealerTableId]),
+	emysql:as_record(Result,dbRound,record_info(fields,dbRound)).
 		
