b48bdfe84107a781bb1a4127ddd9ca9d92a6faea
erlangxk <erlang@viossolution.com>
2014-05-28 10:52:49 +0800

    remove the status constants.

--- include/round.hrl
+++ include/round.hrl
@@ -1,11 +1,6 @@
 -ifndef(ROUND_HRL).
 -define(ROUND_HRL,true).
 
--define(IDLE,0).
--define(BETTING,1).
--define(DEALING,2).
--define(DONE,3).
-
 -record(round,{id,shoeIndex,roundIndex,createTime}).
 
 -endif.
