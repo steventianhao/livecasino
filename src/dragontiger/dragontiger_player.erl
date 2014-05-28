1ca1322fcd01cdde311fe1e24ab831364816ebf0
erlangxk <erlang@viossolution.com>
2014-05-28 11:36:05 +0800

    remove unused hrl file

--- src/dragontiger/dragontiger_player.erl
+++ src/dragontiger/dragontiger_player.erl
@@ -1,7 +1,6 @@
 -module(dragontiger_player).
 -behavior(gen_server).
 -include("user.hrl").
--include("db.hrl").
 
 -export([init/1,handle_call/3,handle_cast/2,handle_info/2,terminate/2,code_change/3]).
 -export([bet/3,start_link/4]).
