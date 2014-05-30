81c6c2b5e75125881f454f47015b73b42318d1f5
erlangxk <erlang@viossolution.com>
2014-05-30 17:24:54 +0800

    insert bets into the ets, and do the payouts, not finished, not tested.

--- src/dragontiger/dragontiger_player.erl
+++ src/dragontiger/dragontiger_player.erl
@@ -9,6 +9,18 @@
 -define(GAME_PLAYER_MOD,dragontiger_player_mod).
 -define(CASINO_DB,mysql_casino_master).
 
+insert_bets(BetEts,BetBundleId,Cats,Amounts)->
+	Ts=lists:zipwith(fun(C,A)->{{BetBundleId,C},A,0} end, Cats, Amounts),
+	ets:insert(BetEts,Ts).
+
+payout_bet('$end_of_table',_BetEts,_RatioMap)->
+	ok;
+payout_bet(Key={_,Cat},BetEts,RatioMap)->
+	Ratio=maps:get(Cat,RatioMap),
+	ets:update_element(BetEts,Key,{3,Ratio}),
+	payout_bet(ets:next(BetEts,Key),BetEts,RatioMap).
+payout_bets(BetEts,RatioMap)->
+	payout_bet(ets:first(BetEts),BetEts,RatioMap).
 
 start_link(Server,EventBus,PlayerTableId,User) when is_pid(Server) andalso is_pid(EventBus) andalso is_integer(PlayerTableId) andalso is_record(User,user)->
 	gen_server:start_link(?MODULE,{Server,EventBus,PlayerTableId,User},[]).
@@ -23,17 +35,18 @@ bet(Pid,Cats,Amounts)->
 
 init({Server,EventBus,PlayerTableId,User=#user{id=UserId}})->
 	dragontiger_player_handler:add_handler(EventBus,UserId),
-	BetEts=ets:new(player_bets,[bag]),
+	BetEts=ets:new(player_bets,[set]),
 	{ok,#state{player_table_id=PlayerTableId,user=User,server=Server,eventbus=EventBus,bet_ets=BetEts}}.
 
-handle_call(Event={bet,Cats,Amounts},_From,State=#state{server=Server,user=User,player_table_id=PlayerTableId})->
+handle_call(Event={bet,Cats,Amounts},_From,State=#state{server=Server,user=User,player_table_id=PlayerTableId,bet_ets=BetEts})->
 	lager:info("bet module ~p, event ~p, state ~p",[?MODULE,Event,State]),
 	case dragontiger_game_api:try_bet(Server,Cats,Amounts) of
 		{ok,Tag,RoundId}->
 			lager:info("after try_bet, tag ~p, roundId ~p",[Tag,RoundId]),
 			Bet=casino_bets:create_bet_req(RoundId,User#user.id,PlayerTableId,Cats,Amounts),
 			case mysql_db:user_bet(?CASINO_DB,Bet) of
-				{ok,Bundle}->
+				{ok,Bundle={BetBundleId,_BalanceAfter}}->
+					insert_bets(BetEts,BetBundleId,Cats,Amounts),
 					{reply,{ok,Bundle},State};
 				Error->
 					{reply,Error,State}
@@ -42,19 +55,6 @@ handle_call(Event={bet,Cats,Amounts},_From,State=#state{server=Server,user=User,
 			lager:info("after try_bet, res ~p",[Res]),
 			{reply,error,State}
 	end.
-
-	%% first, test to save the bets into db
-	%%			{ok,#db_bet_res{bet_bundle_id=BetBundleId}}->
-	%%				dragontiger_game_api:bet_succeed(Server,Tag,BetBundleId),
-	%%				ok;
-	%%			Error={error,insufficient_balance} ->
-	%%				dragontiger_game_api:bet_failed(Server,Tag),
-	%%				Error
-	%%		end.
-	%%	Err={error,_}->
-	%%		Err
-	%%end.
-	
 
 handle_cast(Request,State)->
 	lager:error("unexpected Request ~p, State ~p",[Request,State]),
@@ -67,6 +67,16 @@ handle_info({json,Json},State)->
 handle_info({start_bet,_},State=#state{bet_ets=BetEts})->
 	ets:delete_all_objects(BetEts),
 	{noreply,State};
+
+handle_info({commit,{_Table,Cards}},State=#state{bet_ets=BetEts})->
+	%%atom dragontiger should be the payout scheme, when create this process, should be passed in.
+	RatioMap=?GAME_PLAYER_MOD:payout(Cards,dragontiger),
+	payout_bets(BetEts,RatioMap),
+	%%foldl to caculate the payout for each bundle send to db to update 
+	%%foldl to caculate all, send to player for display.
+	{noreply,State};
+
+
 
 handle_info(Info,State)->
 	lager:error("module ~p, Info ~p, State ~p",[?MODULE,Info,State]),
