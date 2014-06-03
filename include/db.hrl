4b987b74191972edf07bc18c52bd6a7d9c44b576
erlangxk <erlang@viossolution.com>
2014-06-03 16:15:30 +0800

    add user_payout mysql call

--- include/db.hrl
+++ include/db.hrl
@@ -5,8 +5,9 @@
 -record(db_round_res,{id,shoe_index,round_index,dealer_id,dealer_table_id,create_time,status,cards}).
 
 -record(db_bet_req,{round_id,player_id,player_table_id,bet_cats,bet_amounts,total_amount}).
--record(db_bet_res,{bet_bundle_id,balance_before,balance_after}).
+-record(db_bet_res,{status,bet_bundle_id,balance_before,balance_after}).
 
 -record(db_payout_req,{round_id,player_id,player_table_id,bet_bundle_ids,payout_amounts,total_amount}).
+-record(db_payout_res,{status,payout_bundle_id,balance_before,balance_after}).
 
 -endif.
--- src/mysql/mysql_db.erl
+++ src/mysql/mysql_db.erl
@@ -1,7 +1,7 @@
 -module(mysql_db).
 
 -export([insert_round/2,update_round/3,update_round/4,load_last_round/2]).
--export([user_bet/2]).
+-export([user_bet/2,user_payout/2]).
 
 -include("db.hrl").
 -include_lib("emysql/include/emysql.hrl").
@@ -32,13 +32,27 @@ load_last_round(Conn,DealerTableId)->
 	emysql:as_record(Result,db_round_res,record_info(fields,db_round_res)).
 
 user_bet(Conn,#db_bet_req{round_id=RoundId,player_id=PlayerId,player_table_id=PlayerTableId,bet_cats=BetCats,bet_amounts=BetAmounts,total_amount=TotalAmount})->
-	Result=emysql:execute(Conn,"call bet(?,?,?,?,?,?)",[RoundId,PlayerId,PlayerTableId,BetCats,BetAmounts,TotalAmount]),
-	case Result of
-		#ok_packet{} ->
-			{error,insufficient_balance};
-		[R=#result_packet{},#ok_packet{}] ->
-			Res=emysql:as_record(R,db_bet_res,record_info(fields,db_bet_res)),
+	R=emysql:execute(Conn,"call bet(?,?,?,?,?,?)",[RoundId,PlayerId,PlayerTableId,BetCats,BetAmounts,TotalAmount]),
+	[Result=#result_packet{},#ok_packet{}]=R,
+	[Res]=emysql:as_record(Result,db_bet_res,record_info(fields,db_bet_res)),
+	case Res#db_bet_res.status of
+		0 ->
 			#db_bet_res{bet_bundle_id=BetBundleId,balance_after=BalanceAfter}=Res,
-			{ok,{BetBundleId,BalanceAfter}}
+			{ok,{BetBundleId,BalanceAfter}};
+		-1 ->
+			{error,player_not_exist};
+		-2 ->
+			{error,insufficient_balance}
 	end.
 
+user_payout(Conn,#db_payout_req{round_id=RoundId,player_id=PlayerId,player_table_id=PlayerTableId,bet_bundle_ids=BetBundleIds,payout_amounts=PayoutAmounts,total_amount=TotalAmount})->
+	R=emysql:execute(Conn,"call payout(?,?,?,?,?,?)",[RoundId,PlayerId,PlayerTableId,BetBundleIds,PayoutAmounts,TotalAmount]),
+	[Result=#result_packet{},#ok_packet{}]=R,
+	[Res]=emysql:as_record(Result,db_payout_res,record_info(fields,db_payout_res)),
+	case Res#db_payout_res.status of
+		0 ->
+			#db_payout_res{payout_bundle_id=PayoutBundleId,balance_after=BalanceAfter}=Res,
+			{ok,{PayoutBundleId,BalanceAfter}};
+		-1 ->
+			{error,player_not_exist}
+	end.
