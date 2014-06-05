9f4f841f5a48dee897c87d7b8f95b7f6200d8361
erlangxk <erlang@viossolution.com>
2014-06-05 11:11:03 +0800

    fix bug, wrong module

--- src/dragontiger/dragontiger_game.erl
+++ src/dragontiger/dragontiger_game.erl
@@ -166,7 +166,7 @@ handle_sync_event(Event={player_join,User=#user{id=UserId},PlayerTableId},From,S
 			{reply,{error,already_joined},StateName,State};
 		_->
 			Result={ok,Pid}=gen_server:start_link(?PLAYER_MOD,{self(),PlayerTableId,User},[]),
-			gen_event:add_handler(EventBus,{?MODULE,UserId},Pid),
+			gen_event:add_handler(EventBus,{?PLAYER_HANDLER_MOD,UserId},Pid),
 			{reply,Result,StateName,State}
 	end;
 
