f697f1b20515da872b50331ec39548c6b5b7e390
erlangxk <erlang@viossolution.com>
2014-06-19 10:28:07 +0800

    add more in application, reduce the setup work

--- src/dragontiger/dragontiger_app.erl
+++ src/dragontiger/dragontiger_app.erl
@@ -5,7 +5,10 @@
 -export([start/2,stop/1]).
 
 start(_Type,_StartArgs)->
+	application:ensure_started(emysql),
+	mysql_casino_master:add_db_pool(),
 	dragontiger_sup:start_link().
 
 stop(_State)->
+	application:stop(emysql),
 	ok.
