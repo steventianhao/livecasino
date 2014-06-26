98b195d72562591660f980e08807dc4c1796a98b
erlangxk <erlang@viossolution.com>
2014-06-26 14:05:52 +0800

    since checking already done on outer API level, then remove redundant checking

--- src/baccarat/baccarat_payout.erl
+++ src/baccarat/baccarat_payout.erl
@@ -17,8 +17,7 @@ result4(Pt,Bt=9) when Pt<Bt ->
 result4(Pt,Bt) when Pt<Bt ->
 	[banker].
 
-reward4(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,
-		?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2}=Cards) when map_size(Cards)==4 ->
+reward4(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2}) ->
 	Pt=casino_card:total([P1,P2]),
 	Bt=casino_card:total([B1,B2]),
 	R1=result4(Pt,Bt),
@@ -34,15 +33,9 @@ reward_morethan4(Bs=[B1,B2|_],Ps=[P1,P2|_],Func)->
 	R3=casino_bets:add_reward(R2,casino_card:is_pair(P1,P2),player_pair),
 	[big|R3].
 
-reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?BANKER_POS_3 := B3,
-		?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2}=Cards,Func) when map_size(Cards)==5 ->
-	reward_morethan4([B1,B2,B3],[P1,P2],Func);
-
-reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?BANKER_POS_3 := B3,
-		?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2,?PLAYER_POS_3 := P3}=Cards,Func) when map_size(Cards)==6->
+reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?BANKER_POS_3 := B3,?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2,?PLAYER_POS_3 := P3},Func)->
 	reward_morethan4([B1,B2,B3],[P1,P2,P3],Func);
-
-reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,
-		?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2,?PLAYER_POS_3 := P3}=Cards,Func) when map_size(Cards)==5->
+reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?BANKER_POS_3 := B3,?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2},Func)->
+	reward_morethan4([B1,B2,B3],[P1,P2],Func);
+reward4plus(#{?BANKER_POS_1 := B1,?BANKER_POS_2 := B2,?PLAYER_POS_1 := P1,?PLAYER_POS_2 := P2,?PLAYER_POS_3 := P3},Func)->
 	reward_morethan4([B1,B2],[P1,P2,P3],Func).
-
