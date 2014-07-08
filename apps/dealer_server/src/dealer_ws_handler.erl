4e480a9b62c33fb4cebbe687f4da0ea0c628cf8f
erlangxk <erlang@viossolution.com>
2014-07-08 18:24:25 +0800

    add some paring logic

--- apps/dealer_server/src/dealer_ws_handler.erl
+++ apps/dealer_server/src/dealer_ws_handler.erl
@@ -11,13 +11,13 @@ websocket_init(_TransportName,Req,_Otps)->
 	{ok,Req,undefined}.
 
 websocket_handle({text,Msg},Req,State)->
-	{reply,{text,<<"That's what she said!",Msg/binary>>},Req,State};
+	{_,Json}=handle_msg(Msg),
+	{reply,{text,Json},Req,State};
 websocket_handle(_Data,Req,State)->
 	{ok,Req,State}.
 
 websocket_info(auth,Req,State)->
-	Json=jsx:encode(#{<<"kind">> => <<"auth">>}),
-	io:format("json is ~p~n",[Json]),
+	Json=jsx:encode([{<<"kind">>,<<"auth">>}]),
 	{reply,{text,Json},Req,State};
 websocket_info(Info,Req,State)->
 	io:format("info is ~p~n",[Info]),
@@ -26,3 +26,15 @@ websocket_info(Info,Req,State)->
 websocket_terminate(Reason,_Req,_State)->
 	io:format("terminated reason is ~p~n",[Reason]),
 	ok.
+
+handle_msg(Msg)->
+	io:format("this is msg we got ~p~n",[Msg]),
+	case jsx:is_json(Msg) of
+		true-> 
+			{ok,handle_json(Msg)};
+		false->
+			{error,jsx:encode([{<<"kind">>,<<"invalid">>}])}
+	end.
+
+handle_json(Msg)->
+	Msg.
