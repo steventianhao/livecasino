f41c61d75606e76f3ab80c69d376a9ecccd4fd04
erlangxk <erlang@viossolution.com>
2014-07-15 16:49:32 +0800

    fix bugs, stupid rules

--- apps/game_server/src/baccarat/baccarat_dealer_mod.erl
+++ apps/game_server/src/baccarat/baccarat_dealer_mod.erl
@@ -53,13 +53,8 @@ validate(Cards=#{?PLAYER_POS_1 :=P1,?PLAYER_POS_2 :=P2,?BANKER_POS_1 :=B1,?BANKE
 	case {map_size(Cards),Cards} of
 		{4,_}->
 			?ANYONEOF(Pt,?TOTAL89) orelse ?ANYONEOF(Bt,?TOTAL89) orelse (?ANYONEOF(Pt,?TOTAL67) andalso ?ANYONEOF(Bt,?TOTAL67));
-		{5,#{?PLAYER_POS_3 := P3}}->
-			P3v=baccarat_card:value(P3),
-			Bt3=(Bt==3 andalso P3v ==8),
-			Bt4=(Bt==4 andalso ?ANYONEOF(P3v,[0,1,8,9])),
-			Bt5=(Bt==5 andalso (not ?ANYONEOF(P3v,[4,5,6,7]))),
-			Bt6=(Bt==6 andalso (not ?ANYONEOF(P3v,?TOTAL67))),
-			Pt < 6 andalso  (Bt3 orelse Bt4 orelse Bt5 orelse Bt6 orelse Bt==7);
+		{5,#{?PLAYER_POS_3 := _}}->
+			Pt < 6 andalso (not ?ANYONEOF(Bt,?TOTAL89));
 		{5,#{?BANKER_POS_3 :=_}}->
 			?ANYONEOF(Pt,?TOTAL67) andalso Bt < 6;
 		{6,#{?PLAYER_POS_3 :=P3,?BANKER_POS_3 :=_}}->
